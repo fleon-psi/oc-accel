abec505ce5435aef3a408573dc8203e2af1f9b12
Filip Leonarski <filip.leonarski@psi.ch>
2021-03-08 12:01:29 +0100

    Commented part to add ETH in create_framework.tcl
    Signed-off-by: Filip Leonarski <filip.leonarski@psi.ch>

--- hardware/setup/create_framework.tcl
+++ hardware/setup/create_framework.tcl
@@ -256,19 +256,19 @@ foreach ip_bd [glob -nocomplain -dir $action_bd_dir */*.bd] {
 }
 
 # Add Ethernet IP
-if { $eth_used == TRUE } {
-  if { $eth_loop_back == TRUE } {
-    puts "                        adding Ethernet loop back  (no MAC)"
-  } else {
-    puts "                        adding Ethernet block design"
-    set_property  ip_repo_paths [concat [get_property ip_repo_paths [current_project]] $ip_dir] [current_project] >> $log_file
-    update_ip_catalog -rebuild -scan_changes >> $log_file
-
+#if { $eth_used == TRUE } {
+#  if { $eth_loop_back == TRUE } {
+#    puts "                        adding Ethernet loop back  (no MAC)"
+#  } else {
+#    puts "                        adding Ethernet block design"
+#    set_property  ip_repo_paths [concat [get_property ip_repo_paths [current_project]] $ip_dir] [current_project] >> $log_file
+#    update_ip_catalog -rebuild -scan_changes >> $log_file
+#
     # Commented below line for make model, uncomment for make image
-    add_files -norecurse  $ip_dir/eth_100G/eth_100G.srcs/sources_1/bd/eth_100G/eth_100G.bd  >> $log_file
-    export_ip_user_files -of_objects  [get_files  $ip_dir/eth_100G/eth_100G.srcs/sources_1/bd/eth_100G/eth_100G.bd] -no_script -sync -force -quiet >> $log_file
-  }
-}
+#    add_files -norecurse  $ip_dir/eth_100G/eth_100G.srcs/sources_1/bd/eth_100G/eth_100G.bd  >> $log_file
+#    export_ip_user_files -of_objects  [get_files  $ip_dir/eth_100G/eth_100G.srcs/sources_1/bd/eth_100G/eth_100G.bd] -no_script -sync -force -quiet >> $log_file
+#  }
+#}
 
 # Add HBM
 if { $hbm_used == TRUE } {
